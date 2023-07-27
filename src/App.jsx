bc7d149c59462a495a4df50471315486ce6c2bfb
KpryKorn <roffinisacha@gmail.com>
2023-07-27 14:20:51 +0200

    Created Grid background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/ui/Hero";
 import Navbar from "./components/ui/Navbar";
+import GridBg from "./components/ui/GridBg";
 
 function App() {
   return (
     <>
+      <GridBg />
       <Navbar />
       <Hero />
     </>
---
+++ src/components/ui/GridBg.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GridBg() {
+  return <div className="grid-bg"></div>;
+}
